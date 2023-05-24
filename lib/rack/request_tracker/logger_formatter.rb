12d59a5506ed63607d1b922f2c788b6a42067c8b
Franco Leonardo Bulgarelli <franco@mumuki.org>
2023-05-24 11:57:49 -0300

    Use process.pid as in original definition

--- lib/rack/request_tracker/logger_formatter.rb
+++ lib/rack/request_tracker/logger_formatter.rb
@@ -3,6 +3,6 @@ class Logger::Formatter
   Format = "%s, [%s #%d] %5s -- %s: [%s] %s\n".freeze
 
   def call(severity, time, progname, msg)
-    Format % [severity[0..0], format_datetime(time), $$, severity, progname, Rack::RequestTracker.compute_tags, msg2str(msg)]
+    Format % [severity[0..0], format_datetime(time), Process.pid, severity, progname, Rack::RequestTracker.compute_tags, msg2str(msg)]
   end
 end
